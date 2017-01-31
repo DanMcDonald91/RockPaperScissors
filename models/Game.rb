cf0ed5e6ed97bed6bc2b0ca6660501015221335d
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-01-31 07:46:46 +0000

    ROCK SCISSORS PAPER

--- models/Game.rb
+++ models/Game.rb
@@ -10,31 +10,31 @@ class Game
   def play()
     #Detail draw Scenario, Player 1 and 2 Win/Loss Scenarios.
 
-    if @player_1 == "rock" && player2 == "rock"
+    if @player_1 == "rock" && @player2 == "rock"
       return " WE HAVE A DRAW!"
       elsif
-    @player_1 == "paper" && player2 == "paper"
+    @player_1 == "paper" && @player2 == "paper"
     return "WE HAVE A DRAW!"
   elsif 
-   @player_1 == "scissors" && player2 == "scissors"
+   @player_1 == "scissors" && @player2 == "scissors"
     return "WE HAVE A DRAW!"
   elsif
-     @player_1 == "rock" && player2 == "scissors"
+     @player_1 == "rock" && @player2 == "scissors"
       return "PLAYER 1 IS VICTORIOUS"
       elsif
-   @player_1 == "paper" && player2 == "scissors"
+   @player_1 == "paper" && @player2 == "scissors"
     return "PLAYER 2 IS VICTORIOUS"
   elsif
-  @player_1 == "scissors" && player_2 == "rock"
+  @player_1 == "scissors" && @player_2 == "rock"
     return "PLAYER 2 IS VICTORIOUS"
 
-  elsif @player_1 == "rock" && player_2 == "paper"
+  elsif @player_1 == "rock" && @player_2 == "paper"
     return "PLAYER 2 IS VICTORIOUS"
 
-  elsif @player_1 == "paper" && player_2 == "rock"
+  elsif @player_1 == "paper" && @player_2 == "rock"
     return "PLAYER 1 IS VICTORIOUS"
 
-  elsif @player_1 =="scissors" && player_2 = "paper"
+  elsif @player_1 =="scissors" && @player_2 = "paper"
     return "PLAYER 1 IS VICTORIOUS"
     end
   end
